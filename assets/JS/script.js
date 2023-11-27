3b41bd5563d5a32e30c6256ba3b20d1846cb088f
fh255 <>
2023-11-27 13:40:25 +0000

    add navigation

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,19 @@
+/* eslint-env es6 */
+/* jshint esversion: 6 */
 // Vriable to hold the questions and user's Answer
 
 let activeQuestion = 1;
-var username = "";
+var username = "Guest";
 const userResponse = {};
 
 // Define the start quiz function
 
 function startQuiz() {
     let usernameInput = document.getElementById("usernameInput");
-    let username = usernameInput.value;
+    if(usernameInput != null) {
+        let username = usernameInput.value;
+        }
+    
 
     // Display welcome message in a div
 
@@ -89,6 +94,7 @@ function getAnswer() {
 
 function calculateScore() {
     storeAnswer();
+
     const correctAnswers = {
         q1: "west-indies",
         q2: "10",
@@ -97,59 +103,49 @@ function calculateScore() {
 
     let scores = 0;
     let questionNumber = 1;
+
     for (const question in correctAnswers) {
-        if (userResponse[question] === correctAnswers[question]) {
-            scores++;
+        if (correctAnswers.hasOwnProperty(question)) {
+            const imgElement = document.createElement("img");
             const labelElement = document.createElement("label");
-            labelElement.innerHTML = questionNumber + userResponse[question];
-            //document.getElementById("imageDiv").appendChild(labelElement);
-            var imgElement = document.createElement("img");
-            imgElement.setAttribute("src", "assets/images/correct.png");
-            //document.getElementById("imageDiv").appendChild(imgElement);
+            labelElement.innerHTML = `${questionNumber}${userResponse[question]}`;
+
+            imgElement.setAttribute(
+                "src",
+                userResponse[question] === correctAnswers[question] ? "assets/images/correct.png"
+                    : "assets/images/incorrect.png"
+            );
+
             imgElement.style.width = "100%";
             imgElement.style.height = "100%";
-            // Create a container div to hold both label and image
-            const containerDiv = document.createElement('div');
-            containerDiv.style.display = 'flex'; // Set the display to flex
-            containerDiv.style.alignItems = 'center'; // Center items vertically
+
+            const containerDiv = document.createElement("div");
+            containerDiv.style.display = "flex";
+            containerDiv.style.alignItems = "center";
 
             containerDiv.appendChild(labelElement);
             containerDiv.appendChild(imgElement);
 
-            // Append the container div to the "imageDiv"
             document.getElementById("imageDiv").appendChild(containerDiv);
-        } else {
-            const labelElement = document.createElement("label");
-            labelElement.innerHTML = questionNumber + userResponse[question];
-            //document.getElementById("imageDiv").appendChild(labelElement);
-            var imgElement = document.createElement("img");
-            imgElement.setAttribute("src", "assets/images/incorrect.png");
-            //document.getElementById("imageDiv").appendChild(imgElement);
-            imgElement.style.width = "100%";
-            imgElement.style.height = "100%";
-            // Create a container div to hold both label and image
-            const containerDiv = document.createElement('div');
-            containerDiv.style.display = 'flex'; // Set the display to flex
-            containerDiv.style.alignItems = 'center'; // Center items vertically
 
-            containerDiv.appendChild(labelElement);
-            containerDiv.appendChild(imgElement);
+            if (userResponse[question] === correctAnswers[question]) {
+                scores++;
+            }
 
-            // Append the container div to the "imageDiv"
-            document.getElementById("imageDiv").appendChild(containerDiv);
+            questionNumber++;
         }
-        questionNumber++;
     }
 
     const resultText = `You scored ${scores} out of 3`;
     document.getElementById("result-text").textContent = resultText;
 
-    // Hide questions and display result
     document.querySelectorAll(".question").forEach((question) => {
         question.style.display = "none";
     });
+
     document.getElementById("result").style.display = "block";
 }
+
 
 // Show the first question initially
 
